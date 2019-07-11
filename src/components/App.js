87cca625b95f322adc5ac6d0c5806a6b88f82eea
nprofilovic <nprofilovic@gmail.com>
2019-07-11 13:46:56 +0200

    Delete some files and make new one

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentBox from './CommentBox';
+import ComponentList from './CommentList';
+
+export default () => {
+    return(
+        <div>
+            <ComponentBox />
+            <ComponentList />
+        </div>
+    )
+}
